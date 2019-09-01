e909550ef288bf44f752047603057c876316d8ef
Daldek <piotr.de.bever@gmail.com>
2019-09-01 13:50:27 +0200

    From Python 2.x to 3.x, from Polish to English. Some features have been removed

--- IslandSeeker.py
+++ IslandSeeker.py
@@ -1,51 +1,45 @@
-# -*- coding: cp1250 -*-
-plik = open('H:\! temp\Map_100x100_v40.txt', 'r')
-linie = plik.readlines() #zwraca tablice z kolejnymi linijkami.
-plik.close()
-wysokosci = []
-for i in linie: #dla kazdego wiersza
-    rozdzielona = i.split()
-    nowa = []
-    for j in rozdzielona: #dla kazdej liczby
-        nowa.append(float(j))
-    wysokosci.append(nowa)
+input_file = open('E:\OneDrive\Dokumenty\Inne\pyton\IslandSeeker\IslandSeeker\Matrix2.txt', 'r')
+analyzed_file = input_file.readlines()[1:]  # creates a new list in which each verse is an element
+input_file.close()
 
-h = float(raw_input('Podaj rzedna h wody ')) #pobiera rzedna wysokosci
+elevation_matrix = []
 
-r = [] #takie wyspy, dla ktorych nie znamy jeszcze sasiadow
-wyspy = 0 #licznik wysp
+for each_verse in analyzed_file:
+    separated_verse = each_verse.split()  # each element (verse) of the analyzed_file list becomes a set of elements
+    data_conventer = []
+    for each_cell in separated_verse:
+        data_conventer.append(float(each_cell))  # converts each element to a floating point and adds to the
+        # new list
+    elevation_matrix.append(data_conventer)
 
-m = len(wysokosci[0]) #m liczba kolumn
-n = len(wysokosci) #n liczba wierszy
+while True:
+    try:
+        lower_boundary_condition = float(input("Insert the value of the lower boundary condition "))
+        upper_boundary_condition = float(input("Insert the value of the upper boundary condition "))
+        break
+    except ValueError:
+        print("wrong data type")
 
-for y in range(n):
-    for x in range(m):
-        if wysokosci[y][x] == 'q' or wysokosci[y][x] == 'e':  # q to woda e to teren
-            pass #juz odwiedzone
+discoverer = []  # creates an empty list with cells that do not yet have neighbors
+number_of_islands = 0  # this variable counts the number of founded islands
+number_of_cells = 0
+
+vertical_length = len(elevation_matrix[0])
+horizontal_length = len(elevation_matrix)
+
+for horizontal_movement in range(horizontal_length):
+    for vertical_movement in range(horizontal_length):
+        if elevation_matrix[horizontal_movement][vertical_movement] == 'rejected' \
+                or elevation_matrix[horizontal_movement][vertical_movement] == 'approved':
+            pass
         else:
-            if h > wysokosci[y][x]: #znaleziono wode
-                wysokosci[y][x] = 'q' #q to woda
-            else: #znaleziono teren
-                wysokosci[y][x] = 'e' #e to teren
-                r.append((y,x)) #takie wyspy, dla ktorych nie znamy jeszcze sasiadow
-                wyspy = wyspy +1
-                while r != []: # != dopoki pozostaja niezbadane fragmety wyspy
-                    t = r[0] #teraz badamy sasiadow "t"
-                    r.remove(t)
-                    sasiedzi = [(t[0]-1, t[1]), (t[0]+1, t[1]), (t[0], t[1]-1), (t[0], t[1]+1)] #(y,x)
-                    for i in sasiedzi: #i[0] to y, i[1] to x
-                        ix = i[1]
-                        iy = i[0]
-                        if iy < 0 or iy >= n or ix < 0 or ix >= m: #wartosci poza tablica (mapa)
-                            pass
-                        elif wysokosci[iy][ix] == 'q' or wysokosci[iy][ix] == 'e': #juz odwiedzony fragment wyspy
-                            pass
-                        else:
-                            if h > wysokosci[iy][ix]:
-                                wysokosci[iy][ix] = 'q'
-                            else:
-                                wysokosci[iy][ix] = 'e'
-                                r.append((iy,ix))
+            if lower_boundary_condition > elevation_matrix[horizontal_movement][vertical_movement]:
+                elevation_matrix[horizontal_movement][vertical_movement] = 'rejected'
+            elif upper_boundary_condition < elevation_matrix[horizontal_movement][vertical_movement]:
+                elevation_matrix[horizontal_movement][vertical_movement] = 'rejected'
+            else:
+                elevation_matrix[horizontal_movement][vertical_movement] = 'approved'
+                discoverer.append((horizontal_movement, vertical_movement))
+                number_of_islands += 1
 
-
-print 'Przy wysokosci ', h, ' jest ', wyspy, ' wysp'
+print(number_of_islands)
