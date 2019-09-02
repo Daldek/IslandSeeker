e425f787d78d9a7dc9c8e074311ee728ec1726b7
Daldek <piotr.de.bever@gmail.com>
2019-09-02 06:50:10 +0200

    Variables have been renamed

--- IslandSeeker.py
+++ IslandSeeker.py
@@ -1,17 +1,30 @@
+import time
+
 input_file = open('Matrix.txt', 'r')
 analyzed_file = input_file.readlines() # [6:]  creates a new list in which each verse is an element.
 # Skips 6 header lines (In Esri grid data, the first six lines indicate the reference of the grid)
 input_file.close()
 
-elevation_matrix = []
+elevation_array = []  # a new list that will contain converted (float) values from the input_file
+data_converter = []
 
-for each_verse in analyzed_file:
-    separated_verse = each_verse.split()  # each element (verse) of the analyzed_file list becomes a set of elements
-    data_conventer = []
+'''
+The code retrieves each line from the input_file, splits the content into elements and converts to float points
+'''
+
+for each_row in analyzed_file:
+    separated_verse = each_row.split()  # each element (row) of the analyzed_file list becomes a set of elements
     for each_cell in separated_verse:
-        data_conventer.append(float(each_cell))  # converts each element to a floating point and adds to the
+        data_converter.append(float(each_cell))  # converts each element to a floating point and adds to the
         # new list
-    elevation_matrix.append(data_conventer)
+    elevation_array.append(data_converter)
+
+del analyzed_file
+del data_converter
+
+'''
+The code retrieves the upper and lower boundary condition values and checks whether the data type is valid
+'''
 
 while True:
     try:
@@ -19,28 +32,35 @@ while True:
         upper_boundary_condition = float(input("Insert the value of the upper boundary condition "))
         break
     except ValueError:
-        print("wrong data type")
+        print("Wrong data type")
 
-discoverer = []  # creates an empty list with cells that do not yet have neighbors
-number_of_islands = 0  # this variable counts the number of founded islands
-number_of_cells = 0
+'''
+Main part of the script. Values are checked cell by call until the correct value is found. Subsequently the surrounding
+cells are checked.
+'''
 
-vertical_length = len(elevation_matrix[0])
-horizontal_length = len(elevation_matrix)
+base_cell = []  # creates an empty list with a cell that is checked for adjacent valid cells
+# number_of_islands = 0 counts the number of continuous areas with valid cells
+number_of_cells = 0  # counts the total number of valid cells
 
-for horizontal_movement in range(horizontal_length):
-    for vertical_movement in range(horizontal_length):
-        if elevation_matrix[horizontal_movement][vertical_movement] == 'rejected' \
-                or elevation_matrix[horizontal_movement][vertical_movement] == 'approved':
+vertical_length = len(elevation_array[0])  # checks how many items are in a row
+horizontal_length = len(elevation_array)  # checks total number of rows
+
+for horizontal_position in range(horizontal_length):
+    for vertical_position in range(vertical_length):
+        if elevation_array[horizontal_position][vertical_position] == 'rejected' \
+                or elevation_array[horizontal_position][vertical_position] == 'approved':
             pass
         else:
-            if lower_boundary_condition > elevation_matrix[horizontal_movement][vertical_movement]:
-                elevation_matrix[horizontal_movement][vertical_movement] = 'rejected'
-            elif upper_boundary_condition < elevation_matrix[horizontal_movement][vertical_movement]:
-                elevation_matrix[horizontal_movement][vertical_movement] = 'rejected'
+            if lower_boundary_condition > elevation_array[horizontal_position][vertical_position]:
+                elevation_array[horizontal_position][vertical_position] = 'rejected'
+            elif upper_boundary_condition < elevation_array[horizontal_position][vertical_position]:
+                elevation_array[horizontal_position][vertical_position] = 'rejected'
             else:
-                elevation_matrix[horizontal_movement][vertical_movement] = 'approved'
-                discoverer.append((horizontal_movement, vertical_movement))
-                number_of_islands += 1
+                elevation_array[horizontal_position][vertical_position] = 'approved'
+                base_cell.append((horizontal_position, vertical_position))
+                number_of_cells += 1
 
-print(number_of_islands)
+print(number_of_cells)
+
+time.sleep(3)
