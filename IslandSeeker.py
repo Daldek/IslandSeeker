5c4d8e6b3e79cbb2d838757631318f065579ef74
Daldek <piotr.de.bever@gmail.com>
2019-09-07 22:40:18 +0200

    Bugs fixed. The number of rows and columns is now correctly calculated. New feature - the number of relevant areas is coundted

--- IslandSeeker.py
+++ IslandSeeker.py
@@ -6,7 +6,6 @@ analyzed_file = input_file.readlines() # [6:]  creates a new list in which each 
 input_file.close()
 
 elevation_array = []  # a new list that will contain converted (float) values from the input_file
-data_converter = []
 
 '''
 The code retrieves each line from the input_file, splits the content into elements and converts to float points
@@ -14,13 +13,13 @@ The code retrieves each line from the input_file, splits the content into elemen
 
 for each_row in analyzed_file:
     separated_verse = each_row.split()  # each element (row) of the analyzed_file list becomes a set of elements
+    data_converter = []
     for each_cell in separated_verse:
         data_converter.append(float(each_cell))  # converts each element to a floating point and adds to the
         # new list
     elevation_array.append(data_converter)
 
 del analyzed_file
-del data_converter
 
 '''
 The code retrieves the upper and lower boundary condition values and checks whether the data type is valid
@@ -39,28 +38,49 @@ Main part of the script. Values are checked cell by call until the correct value
 cells are checked.
 '''
 
-base_cell = []  # creates an empty list with a cell that is checked for adjacent valid cells
-# number_of_islands = 0 counts the number of continuous areas with valid cells
+base_cells = []  # creates an empty list with cells that is checked for adjacent valid cells
+number_of_islands = 0  # counts the number of continuous areas with valid cells
 number_of_cells = 0  # counts the total number of valid cells
 
-vertical_length = len(elevation_array[0])  # checks how many items are in a row
-horizontal_length = len(elevation_array)  # checks total number of rows
+horizontal_length = len(elevation_array[0])  # checks how many items are in a row
+vertical_length = len(elevation_array)  # checks total number of rows
 
-for horizontal_position in range(horizontal_length):
-    for vertical_position in range(vertical_length):
-        if elevation_array[horizontal_position][vertical_position] == 'rejected' \
-                or elevation_array[horizontal_position][vertical_position] == 'approved':
-            pass
+for vertical_position in range(vertical_length):
+    for horizontal_position in range(horizontal_length):
+        if elevation_array[vertical_position][horizontal_position] == 'rejected' or\
+                elevation_array[vertical_position][horizontal_position] == 'approved':
+            pass  # this cell has already been checked
         else:
-            if lower_boundary_condition > elevation_array[horizontal_position][vertical_position]:
-                elevation_array[horizontal_position][vertical_position] = 'rejected'
-            elif upper_boundary_condition < elevation_array[horizontal_position][vertical_position]:
-                elevation_array[horizontal_position][vertical_position] = 'rejected'
+            if lower_boundary_condition > elevation_array[vertical_position][horizontal_position] or\
+                    upper_boundary_condition < elevation_array[vertical_position][horizontal_position]:
+                elevation_array[vertical_position][horizontal_position] = 'rejected'
             else:
-                elevation_array[horizontal_position][vertical_position] = 'approved'
-                base_cell.append((horizontal_position, vertical_position))
+                elevation_array[vertical_position][horizontal_position] = 'approved'
+                base_cells.append((vertical_position, horizontal_position))
+                number_of_islands += 1
                 number_of_cells += 1
+                while base_cells != []:
+                    base_cell = base_cells[0]
+                    base_cells.remove(base_cell)
+                    adjacent_cells = [(base_cell[0]-1, base_cell[1]), (base_cell[0]+1, base_cell[1]),\
+                                      (base_cell[0], base_cell[1]-1), (base_cell[0], base_cell[1]+1)]
+                    for cell in adjacent_cells:
+                        y = cell[0]
+                        x = cell[1]
+                        if y < 0 or y >= vertical_length or x < 0 or x >= horizontal_length:
+                            pass
+                        elif elevation_array[y][x] == 'rejected' or elevation_array[y][x] == 'approved':
+                            pass
+                        else:
+                            if lower_boundary_condition > elevation_array[y][x] or\
+                                    upper_boundary_condition < elevation_array[y][x]:
+                                elevation_array[y][x] = 'rejected'
+                            else:
+                                elevation_array[y][x] = 'approved'
+                                base_cells.append((y, x))
+                                number_of_cells += 1
 
-print(number_of_cells)
+print("Number of appropriate cells: " + str(number_of_cells))
+print("Number of islands: " + str(number_of_islands))
 
 time.sleep(3)
